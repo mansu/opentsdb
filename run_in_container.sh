16a6ddd589368471e1a8ee2103a452ff98ce0d8a
jinghan <jinghan@pinterest.com>
2017-10-31 17:03:32 -0700

    modify docker file

--- run_in_container.sh
+++ run_in_container.sh
@@ -16,16 +16,4 @@ export PARENT_DIR="$(dirname $DIR)"
 LOG_DIR=/var/log/${SERVICENAME}
 CP=${PARENT_DIR}/*jar
 
-exec java -server -Xmx${HEAP_SIZE} -Xms${HEAP_SIZE} -XX:NewSize=${NEW_SIZE} -XX:MaxNewSize=${NEW_SIZE} \
-    -verbosegc -Xloggc:${LOG_DIR}/gc.log \
-    -XX:+UnlockDiagnosticVMOptions -XX:ParGCCardsPerStrideChunk=4096 -XX:+PerfDisableSharedMem \
-    -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=100 -XX:GCLogFileSize=2M \
-    -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintClassHistogram \
-    -XX:+HeapDumpOnOutOfMemoryError -XX:+UseParNewGC \
-    -XX:+UseConcMarkSweepGC -XX:CMSInitiatingOccupancyFraction=60 -XX:+UseCMSInitiatingOccupancyOnly \
-    -XX:ErrorFile=${LOG_DIR}/jvm_error.log -Dnetworkaddress.cache.ttl=60 -Djava.net.preferIPv4Stack=true \
-    -cp ${CP} -Dlogback.configurationFile=${LOGBACK_FILE} -Dstage_config=${STAGE_CONFIG_FILE} \
-    -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=10102 \
-    -Dfile.encoding=UTF-8\
-    ${JAVA_MAIN} --config=yuvi.conf
+exec java -Dlogback.configurationFile=logback.xml -jar opentsdb-2.3.0-fat.jar --config=yuvi.conf
