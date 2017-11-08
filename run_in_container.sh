31f0fe4c75f999008839c8ab9e224562dc5ee038
Suman Karumuri <skarumuri@pinterest.com>
2017-11-07 17:31:20 -0800

    Pass config file as an env variable.
     More JVM params.

--- run_in_container.sh
+++ run_in_container.sh
@@ -16,4 +16,4 @@ export PARENT_DIR="$(dirname $DIR)"
 LOG_DIR=/var/log/${SERVICENAME}
 CP=${PARENT_DIR}/*jar
 
-exec java -Dlogback.configurationFile=logback.xml -jar opentsdb-2.3.0-fat.jar --config=yuvi.conf
+exec java -Xmx30g -verbosegc -Xloggc:/var/log/metron/gc.log -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -XX:+PrintGCDateStamps -XX:+PrintClassHistogram -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=100 -XX:GCLogFileSize=2M -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC -XX:MaxGCPauseMillis=100 -XX:G1ReservePercent=10 -XX:ConcGCThreads=4 -XX:ParallelGCThreads=4 -XX:G1HeapRegionSize=8m -XX:InitiatingHeapOccupancyPercent=70 -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=10102 -agentpath:/opt/yjp/bin/linux-x86-64/libyjpagent.so=disablestacktelemetry,exceptions=disable,delay=10000,usedmem=69,logdir=/var/log/data -Dlogback.configurationFile=logback.xml -jar opentsdb-2.3.0-fat.jar --config=${STAGE_CONFIG_FILE}
